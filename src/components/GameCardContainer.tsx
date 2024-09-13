c203907647930b964bf3c0a9c0910dfa6b18a608
Rohit <rohitmathew76@gmail.com>
2024-09-13 14:09:51 +0530

    Display genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="290px" borderRadius="10px" overflow="hidden">
+    <Box borderRadius="10px" overflow="hidden">
       {children}
     </Box>
   );
